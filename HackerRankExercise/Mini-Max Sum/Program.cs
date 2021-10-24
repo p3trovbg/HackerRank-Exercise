c186aaa3c9b4a07cdd74cd97007d945f01911bc8
Georgi Petrov <78408524+p3trovbg@users.noreply.github.com>
2021-10-24 16:02:09 +0300

    Exercise

---
+++ HackerRankExercise/Mini-Max Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Mini_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var array = Console.ReadLine().Split().Select(long.Parse).ToArray();
+           
+            var secondSum = array.Where(x => x != array.Min()).Sum();
+            var firstSum = array.Where(x => x != array.Max()).Sum();
+            if (firstSum == 0 && secondSum == 0)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    firstSum += array[i];
+                    secondSum += array[i];
+                }
+            }
+            Console.WriteLine($"{firstSum} {secondSum}");
+        }
+    }
+}
