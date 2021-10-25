2729148f08df31231e4a16ea739681fb4a5683f5
Georgi Petrov <78408524+p3trovbg@users.noreply.github.com>
2021-10-25 19:43:04 +0300

    Exercise

---
+++ HackerRankExercise/Birthday Cake Candles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Birthday_Cake_Candles
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var candles = new Dictionary<int, int>();
+            foreach (var digit in list)
+            {
+                if (!candles.ContainsKey(digit))
+                {
+                    candles.Add(digit, 0);
+                }
+                candles[digit]++;
+            }
+            Console.WriteLine(candles.OrderByDescending(x => x.Value).FirstOrDefault().Value);
+        }
+    }
+}
---
+++ HackerRankExercise/Time Conversion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using System.Linq;
+
+namespace Time_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = Console.ReadLine().Split(new string[] {":", "PM"}, StringSplitOptions.RemoveEmptyEntries);
+            int x = int.Parse(time[0]);
+            if (x == 12)
+            {
+                x = 0;
+            }
+            else
+            {
+                x += 12;
+            }
+            Console.WriteLine($"{x}:{time[1]}:{time[2]}");
+              
+        }
+    }
+}
