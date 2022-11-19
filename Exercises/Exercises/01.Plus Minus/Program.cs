2941fbbeef9b082dc70b7edca0dfc68b474bba20
George <p3trovsit@gmail.com>
2022-11-19 16:30:11 +0200

    Adding "Plus Minus" problem

---
+++ Exercises/Exercises/01.Plus Minus/Program.cs
@@ -0,0 +1,51 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        var negativeDigits = new List<int>();
+        var positiveDigits = new List<int>();
+        var zeros = new List<int>();
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            var digit = arr[i];
+            if (digit > 0)
+            {
+                positiveDigits.Add(digit);
+            }
+            else if (digit < 0)
+            {
+                negativeDigits.Add(digit);
+            }
+            else
+            {
+                zeros.Add(digit);
+            }
+        }
+
+        int size = arr.Count;
+        Console.WriteLine($"{(decimal)positiveDigits.Count / size:f6}");
+        Console.WriteLine($"{(decimal)negativeDigits.Count / size:f6}");
+        Console.WriteLine($"{(decimal)zeros.Count / size:f6}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
